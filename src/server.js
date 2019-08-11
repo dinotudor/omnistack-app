69fdba00e361b4502c83c6c2cb0ad6b48377cfee
Dino Tudor <dinotudor@gmail.com>
2019-08-11 13:56:36 +0200

    add socket.io

--- src/server.js
+++ src/server.js
@@ -7,15 +7,21 @@ const cors = require('cors');
 const routes = require('./routes');
 
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('New conection', socket.id)
+})
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-bejzn.mongodb.net/omnistack?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
 
